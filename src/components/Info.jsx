37f2d49337dc79db203737889123bc1161228426
pequito193 <vitorspequito@gmail.com>
2022-09-02 11:19:53 +0100

    Remove unnecessary component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,7 +7,7 @@ class Info extends Component {
     }
 
     render() {
-        
+
         const {firstName, lastName, jobTitle, phoneNumber, email, linkedIn, gitHub, personalWebsite} = this.props;
 
         return (
--- src/components/ParentWrapper.jsx
+++ src/components/ParentWrapper.jsx
@@ -1,13 +1,13 @@
 import React, {Component} from "react";
 import Info from "./Info";
-import Editor from "./Editor";
+import "./../styles/Editor.css"
 
 class ParentWrapper extends Component {
     
     constructor(props) {
         super(props);
 
-        this.handleInput = this.handleInput.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
 
         this.state = {
             firstName: '',
@@ -21,16 +21,30 @@ class ParentWrapper extends Component {
         };
     }
 
-    handleInput(input) {
-        this.setState({[this.prop]: input});
+    handleInputChange(e) {
+        const inputField = e.target.name;
+        const value = e.target.value;
+        this.setState({[inputField]: value});
     }
 
     render() {
-        console.log(this.state)
         return (
         <React.Fragment>
             <Info />
-            <Editor  handleInputChange={this.handleInput} />
+            <div className="editor-wrapper">
+                <h2 className="editor-title">Personal Info</h2>
+                <div className="inputs">
+                    <input className="input first-name" name="firstName" onChange={this.handleInputChange} type="text" placeholder="First Name" />
+                    <input className="input last-name" name="lastName" onChange={this.handleInputChange} type="text" placeholder="Last Name" />
+                    <input className="input job-title" name="jobTitle" onChange={this.handleInputChange} type="text" placeholder="Job Title" />
+                    <input className="input phone-number" name="phoneNumber" onChange={this.handleInputChange} type="tel" placeholder="Phone Number" />
+                    <input className="input email" name="email" onChange={this.handleInputChange} type="email" placeholder="Email" />
+                    <input className="input linked-in" name="linkedIn" onChange={this.handleInputChange} type="url" placeholder="Linked In" />
+                    <input className="input git-hub" name="gitHub" onChange={this.handleInputChange} type="url" placeholder="Github" />
+                    <input className="input personal-website" name="personalWebsite" onChange={this.handleInputChange} type="url" placeholder="Personal Website" />
+                </div>
+                <h2 className="editor-title">Add Languages</h2>
+            </div>
         </React.Fragment>
         )    
     }
